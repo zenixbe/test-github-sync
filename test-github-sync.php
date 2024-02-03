3f53721b33074789cd9f2f380ed7349e573cfcc9
zenixbe <arnedm85@gmail.com>
2024-02-03 16:44:16 +0100

    vers

--- test-github-sync.php
+++ test-github-sync.php
@@ -16,7 +16,7 @@
 // Function to output "Hello World" text
 function add_hello_world_text()
 {
-    echo '<div id="hello-world-text" style="margin-top: 100px;">Hello World sync</div>';
+    echo '<div id="hello-world-text" style="margin-top: 100px;">Hello didier sync</div>';
 }
 
 // Hook the function to an action that runs before the header

18df5515701e33449ac0fcd16106333f2db48d70
zenixbe <arnedm85@gmail.com>
2024-02-03 16:43:22 +0100

    vers

--- test-github-sync.php
+++ test-github-sync.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Hello world header
  * Plugin URI:        https://github.com/zenixbe/test-github-sync
  * Description:       A plugin to automatically update GitHub, Bitbucket or GitLab hosted plugins and themes. It also allows for remote installation of plugins or themes into WordPress.
- * Version:           10.0.1
+ * Version:           10.0.2
  * Author:            Andy Fragen
  * License:           MIT
  * Text Domain:       helloworldheader
@@ -25,7 +25,7 @@ add_action('wp_body_open', 'add_hello_world_text');
 // Function to add custom CSS
 function hello_world_custom_styles()
 {
-    echo '<style>#hello-world-text { margin-top: 100px; color:white; }</style>';
+    echo '<style>#hello-world-text { margin-top: 100px; color:blue; }</style>';
 }
 
 // Hook for adding custom styles
